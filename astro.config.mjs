17069bfb533af29de559944f19d21fb0f643d4ae
Elizarov <elizar@mail.ru>
2026-05-07 12:10:27 +0500

    fix: re-deploy with CSS

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-export default defineConfig({
-  output: 'static',
-  integrations: [tailwind()],
-  site: 'https://yourdomain.kz',
-});
